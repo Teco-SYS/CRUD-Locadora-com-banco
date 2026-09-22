774e87caf4997261e82361708d03832a47f45880
Mateus Rodrigues <gdmateus55@gmail.com>
2026-09-22 19:30:17 -0300

    Delete 2026-2sem-itapevi-ds3n-crud-locadora-com-banco-main directory

--- 2026-2sem-itapevi-ds3n-crud-locadora-com-banco-main/src/routes/testRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-
-const testController = require("../controllers/testController")
-
-router.get("/", testController.testConnection)
-router.get("/tables", testController.listTables)
-
-module.exports = router
--- 2026-2sem-itapevi-ds3n-crud-locadora-com-banco-main/src/server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app")
-
-const PORTA = 3000
-
-app.listen(PORTA, () => {
-    console.log(`Servidor rodando em http://localhost:${PORTA}`)
-})
